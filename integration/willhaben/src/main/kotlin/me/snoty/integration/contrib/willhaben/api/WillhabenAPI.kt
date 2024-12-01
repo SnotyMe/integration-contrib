b6c1fba0f66aded907e496417111241b4f573dd0
SIMULATAN <simulatan2319@gmail.com>
2024-12-01 22:37:49 +0100

    fix(Willhaben): expired ad

--- integration/willhaben/src/main/kotlin/me/snoty/integration/contrib/willhaben/api/WillhabenAPI.kt
+++ integration/willhaben/src/main/kotlin/me/snoty/integration/contrib/willhaben/api/WillhabenAPI.kt
@@ -1,30 +1,52 @@
 package me.snoty.integration.contrib.willhaben.api
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.*
+import io.ktor.client.plugins.RedirectResponseException
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import io.ktor.http.HttpHeaders
+import io.ktor.http.parseUrl
 import kotlinx.serialization.json.*
 import org.koin.core.Koin
 import org.koin.core.annotation.Single
 
 interface WillhabenAPI {
-	suspend fun fetchListing(url: String): WillhabenListing
+	suspend fun fetchListing(url: String): WillhabenListing?
 }
 
 @Single
-class WillhabenAPIImpl(private val httpClient: HttpClient, koin: Koin) : WillhabenAPI {
+class WillhabenAPIImpl(snotyClient: HttpClient, koin: Koin) : WillhabenAPI {
 	private val json by koin.inject<Json>()
-
-	companion object {
-		private val pattern = "https://(www\\.)?willhaben\\.at/.*".toRegex()
+	private val httpClient = snotyClient.config {
+		followRedirects = false
 	}
+
+	val logger = KotlinLogging.logger {}
 
 	private fun JsonObject.el(name: String) = this[name] ?: throw IllegalArgumentException("Missing required field: $name in $this")
 
-	override suspend fun fetchListing(url: String): WillhabenListing {
-		if (!url.matches(pattern)) throw IllegalArgumentException("Not a willhaben url: $url")
+	override suspend fun fetchListing(url: String): WillhabenListing? {
+		val mappedUrl = parseUrl(url) ?: throw IllegalArgumentException("Invalid URL: $url")
+		if (mappedUrl.host != "www.willhaben.at") throw IllegalArgumentException("Not a willhaben URL: $url (are you missing the www?)")
+		if (mappedUrl.segments.firstOrNull() != "iad" || !mappedUrl.segments.contains("d")) throw IllegalArgumentException("Not a listing URL: $url")
 
-		val html = httpClient.get(url).bodyAsText()
+		val response = try {
+			httpClient.get(mappedUrl)
+		} catch (redirect: RedirectResponseException) {
+			val location = redirect.response.headers[HttpHeaders.Location] ?: throw redirect
+
+			if (location.contains("?fromExpiredAdId=")) {
+				logger.warn { "Listing $url is expired" }
+				return null
+			}
+			if (!location.contains("/d/")) throw IllegalArgumentException("Invalid redirect location: $location")
+
+			// redirect may be valid (http -> https?) - try the new URL
+			httpClient.get(location)
+		}
+
+		val html = response.bodyAsText()
 		val jsonRaw = html.substringAfter("<script id=\"__NEXT_DATA__\" type=\"application/json\">").substringBefore("</script>")
 		val props = json.parseToJsonElement(jsonRaw).jsonObject.el("props").jsonObject.el("pageProps").jsonObject
 		val advertDetails = props.el("advertDetails").jsonObject
