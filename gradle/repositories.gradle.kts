6ffdad2c3d3f8c459806834872ba7bdd8e1ff51d
SIMULATAN <jakob@simulatan.me>
2025-01-22 12:36:18 +0100

    chore(deps): update dependency me.snoty:versions  to v0.0.4

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,12 +1,13 @@
 dependencyResolutionManagement {
 	repositories {
+		mavenLocal()
 		mavenCentral()
 		maven("https://maven.snoty.me/snapshots")
 		gradlePluginPortal()
 	}
 	versionCatalogs {
 		create("snoty") {
-			from("me.snoty:versions:0.0.3")
+			from("me.snoty:versions:0.0.4")
 		}
 	}
 }
