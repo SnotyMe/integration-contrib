e41f2d642f099a7854a62b5152edd6be921dfe52
SIMULATAN <jakob@simulatan.me>
2026-01-30 13:35:03 +0100

    chore(deps)!: update to Snoty v0.7.0

--- gradle/repositories.gradle.kts
+++ gradle/repositories.gradle.kts
@@ -1,12 +1,12 @@
 dependencyResolutionManagement {
 	repositories {
 		mavenCentral()
-		maven("https://maven.snoty.me/snapshots")
+		maven("https://maven.snoty.me/releases")
 		gradlePluginPortal()
 	}
 	versionCatalogs {
 		create("snoty") {
-			from("me.snoty:versions:0.7.0-rc.1")
+			from("me.snoty:versions:0.7.0")
 		}
 	}
 }
